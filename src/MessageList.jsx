c0041077c7801f4e1df857732ffd02618defb144
Sherlaine <sherlaine.lau@gmail.com>
2018-10-30 21:28:55 +0000

    first react components,modularize app, chatbar, message, messagelist

---
+++ src/MessageList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Message from './Message.jsx';
+ class MessageList extends React.Component {
+  render() {
+    return (
+        <main className="messages">
+        <Message />
+        </main>
+    );
+  }
+}
+ export default MessageList;
